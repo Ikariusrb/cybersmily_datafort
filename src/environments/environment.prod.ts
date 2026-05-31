6bb00d55f39e7198376fc52fd6f8296cc77bc139
Ross Becker <ross.becker@gmail.com>
2026-05-31 12:57:46 -0700

    keys for accessing google APIs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   googleDrive: {
-    clientId: '',
-    apiKey: '',
-    appId: '',
+    clientId: '370894403439-kd471a6a57b7dnd423iv3kll5gev47ig.apps.googleusercontent.com',
+    apiKey: 'AIzaSyDzCG5invOqfUZDreHMOdq4tvYDjpYmRmY',
+    appId: '370894403439',
   },
 };
